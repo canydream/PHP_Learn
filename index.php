65042e7f6a3c5eedd36bee1166b99f980f2fa9bb
canydream <36945483+canydream@users.noreply.github.com>
2018-11-02 15:11:39 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>菜鸟教程(runoob.com)</title>
+</head>
+<body>
+
+<h1>我的第一个标题</h1>
+<p><?php echo "Hello World"?></p>
+<a href="http://www.runoob.com">这是一个链接</a>
+</body>
+</html>
